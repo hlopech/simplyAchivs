5d6ab4290ff548b679ee01594c22eca3ca8abdd2
hlopech <nikitastarovojtov42@gmail.com>
2026-03-05 17:27:33 +0100

    add asset

---
+++ app/src/main/java/com/example/simplyachivs/presentation/shop/addAward/AwardImage.kt
@@ -0,0 +1,14 @@
+package com.example.simplyachivs.presentation.shop.addAward
+
+import com.example.simplyachivs.R
+
+enum class AwardImage(val resId: Int, val label: String) {
+    AWARD(R.drawable.award_img, "Награда"),
+    SPORT(R.drawable.sport_target_img, "Спорт"),
+    EDUCATION(R.drawable.education_target_img, "Учёба"),
+    HOLIDAY(R.drawable.holiday_target_img, "Отдых"),
+    COIN(R.drawable.coin_img, "Монета"),
+    STAR(R.drawable.star_icon, "Звезда"),
+    FIRE(R.drawable.fire_icon, "Огонь"),
+    FLAG(R.drawable.flag_icon, "Цель"),
+}
